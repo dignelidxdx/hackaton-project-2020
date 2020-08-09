cfc2817c7a9f3a1c6c57f15e99655ab6461232ae
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-08-09 14:21:39 -0300

    fix logo, sign up, sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :project_interests, path: "interests"
   resources :projects
   devise_for :users
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard', as: 'dashboard'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
